d87afb760449a181d53a6757b627f1aa1e7f48b0
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2022-03-10 13:09:56 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 import Header from "./UI/Header";
 import theme from "./UI/Theme";
 import Home from "./Component/Home";
-import img from './Assets/mainbg1.png'
 import Footer from "./UI/Footer";
 
 
-const useStyles = makeStyles((theme) => ({
-  "@global": {
-    body: {
-          backgroundImage: `url(${img})`,
-    }
-  }
-}))
 
 
 function App() {
-  const classes= useStyles()
   return (
     <ThemeProvider theme={theme}>
       <Router>
