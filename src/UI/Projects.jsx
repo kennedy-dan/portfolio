8118798a14fd10d054d0ffe58e271a7a694ce3bf
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2022-05-16 14:18:53 +0100

    changes

--- src/UI/Projects.jsx
+++ src/UI/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import Ify from "../Assets/ifytry.jpg";
+import football from '../Assets/football-project.png'
 
 const useStyles = makeStyles((theme) => ({
   myWorks: {
@@ -15,16 +16,22 @@ const useStyles = makeStyles((theme) => ({
   workCard: {
     width: "480px",
     height: "290px",
-    boxShadow: "0px 0px 5px 2px #fa9a1c",
+    
+    boxShadow: "0px 0px 5px 5px rgb(26, 25, 25, 0.219)",
     border: "none",
     [theme.breakpoints.only('xs')]:{
       width:'400px',
       height:'220px'
+    },
+    "&:hover":{
+    boxShadow: "0px 0px 9px 9px rgb(26, 25, 25, 0.219)",
+
     }
 
   },
   workCardContainer:{
-    paddingTop:'40px'
+    marginTop:'40px',
+    padding: '20px'
   },
   img: {
     width: "480px",
@@ -48,11 +55,19 @@ const Projects = () => {
       <Grid item container justifyContent="center">
         <p className={classes.myWorks}>My Works</p>
       </Grid>
-      <Grid item container justifyContent="center" className={classes.workCardContainer}>
+      <Grid item container justifyContent="space-between" className={classes.workCardContainer} spacing={2}>
         <div className={classes.workCard}>
           <a href='https://dr-ifeyinwa.vercel.app/'>
             <div>
               <img src={Ify} className={classes.img} alt='ify'/>
+            </div>
+          </a>
+        </div>
+
+        <div className={classes.workCard}>
+          <a href='https://football-flame.vercel.app/'>
+            <div>
+              <img src={football} className={classes.img} alt='ify'/>
             </div>
           </a>
         </div>
